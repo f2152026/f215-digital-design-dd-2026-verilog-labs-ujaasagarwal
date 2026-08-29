e6bfaf1210f68239fa73b1fca933ea8427497eba
ujaasagarwal <ujaas1234@gmail.com>
2026-08-29 15:52:15 +0000

    part b for task 2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -1,3 +1,5 @@
+// FA_Gate.v
+
 module FA_Gate(
   input a,
   input b,
@@ -8,10 +10,10 @@ module FA_Gate(
 
   wire ps, pc1, pc2;
 
-  xor #(2) (ps,  a,   b);
-  and #(2) (pc1, a,   b);
-  xor #(2) (sum, cin, ps);
-  and #(2) (pc2, cin, ps);
-  or  #(2) (cout, pc1, pc2);
+  xor #(2,3) (ps,  a,   b);
+  and #(2,3) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(2,3) (pc2, cin, ps);
+  or  #(2,3) (cout, pc1, pc2);
 
 endmodule
